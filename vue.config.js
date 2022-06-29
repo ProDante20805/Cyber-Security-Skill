95102e9a9e34398234b9e9e67a47db2c0f38ad88
ProDante20805 <pro.guru0114@gmail.com>
2022-06-29 14:35:24 +0800

    Add plugins.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pluginOptions: {
+    i18n: {
+      locale: undefined,
+      fallbackLocale: undefined,
+      localeDir: undefined,
+      enableInSFC: undefined,
+      enableBridge: undefined,
+    },
+  },
 });
